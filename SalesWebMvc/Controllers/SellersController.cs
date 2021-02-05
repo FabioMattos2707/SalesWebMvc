4248456f02b0f101fce5fb5e546adcd6fe523495
FabioMattos2707 <fabio.matos2707@outlook.com>
2021-02-05 13:14:52 -0300

    SellesController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+using SalesWebMvc.Data;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private SalesWebMvcContext _context;
+        public SellersController(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            
+            return View(_context.Seller.ToList());
+        }
+    }
+}
